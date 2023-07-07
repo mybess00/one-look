ca9c37a87d4d79e8d29acc840455c0b7ff65c9a7
mybess00 <betancourtrafael00@gmail.com>
2023-07-07 15:22:50 -0400

    update

--- src/components/paylink/Modal.jsx
+++ src/components/paylink/Modal.jsx
@@ -1,34 +1,24 @@
-import { useRef, useEffect } from "react";
 import QRCode  from "qrcode.react";
 
 export default function Modal ({ title, id, content, picture, copyFunction }) {
-
-  const qrContainerRef = useRef(null)
-  const qrCode = (<QRCode value={content} size='200'
-  imageSettings={{
-    src: `${picture}`,
-    x: undefined,
-    y: undefined,
-    height: 36,
-    width: 36,
-    excavate: true,
-  }}/>)
-
-  useEffect(() => {
-    if (qrContainerRef.current){
-  //    qrCode.append(qrContainerRef)
-    }
-  },[])
-  
+ 
   return (
     <>
       <input type="checkbox" id={`modal-qr-${id}`} className="modal-toggle" />
       <div className="modal">
-        <div className="modal-box relative">
+        <div className="modal-box relative"> 
           <label htmlFor={`modal-qr-${id}`} className="btn btn-primary btn-sm btn-circle absolute right-2 top-2">✕</label>
           <h3 className="text-lg font-bold">{title}</h3>
-          <div className="flex flex-col items-center my-1.5" ref={qrContainerRef}>
-            {qrCode}
+          <div className="flex flex-col items-center my-1.5">
+          <QRCode value={content} size='200'
+            imageSettings={{
+              src: `${picture}`,
+              x: undefined,
+              y: undefined,
+              height: 36,
+              width: 36,
+              excavate: true,
+            }}/>
             <button className="btn btn-primary gap-2 btn-sm mt-6 px-10" onClick={copyFunction}>
               <img src="/images/icons/copy.svg" alt="copy icon" className="w-3.5 h-3.5"/> Copy
             </button>
